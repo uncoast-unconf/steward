c658845f522eac0440d90248c5813d6e8d9d7b29
AmandaRaeISU <amanda.rae.50315@gmail.com>
2019-04-16 14:18:58 -0500

    The whole lunch box

--- R/stw_to_roxygen.R
+++ R/stw_to_roxygen.R
@@ -7,42 +7,57 @@
 #' @export
 #'
 #' @examples
+
 stw_to_roxygen <- function(meta) {
-  lunch_box <-
+
+  top_bread <-
     glue::glue(
-      "#' {meta$title}",
-      "#' ",
-      "#' {meta$description}",
-      "#' ",
-      "#' @format A data frame with {meta$n_row} rows and {meta$n_col} variables:",
-      "#' ",
+        "#' {meta$title}",
+        "#' ",
+        "#' {meta$description}",
+        "#' ",
+        "#' @format A data frame with {meta$n_row} rows and {meta$n_col} variables:",
+        "#' ",
+        "#' \\describe{{ ",
+        .sep = "\n"
+    )
+
+  fillings <- dict_to_roxygen(meta$dictionary)
+
+  bottom_bread <-
+    glue::glue(
+      "#' }}",
       "#' @source {meta$source}",
+      "\"{meta$name}\"",
       .sep = "\n"
     )
 
+  sandwich <-
+    glue::glue_collapse(
+      c(top_bread, fillings, bottom_bread),
+      sep = "\n"
+    )
 
-  lunch_box
+  sandwich
 }
+
 
 dict_to_roxygen <- function(dict) {
 
   make_filling<- function(raw){
     glue::glue(
-      "#'  \\item{{{raw$name}}}{{{raw$description}}}\n"
+      "#'   \\item{{{raw$name}}}{{{raw$description}}}\n"
     )
   }
-
 
   temp <- purrr::transpose(dict)
 
   fillings_processed <- unlist(lapply(temp, make_filling))
 
 
-
 fillings <- glue::glue_collapse(fillings_processed,sep = "\n")
 
 fillings
 }
-
 
 
