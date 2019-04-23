56a3476ea52f10e442010dc342e03fb33b86dfcb
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-04-23 18:02:26 -0500

    Update style

--- R/stw_to_gt.R
+++ R/stw_to_gt.R
@@ -8,10 +8,10 @@
 #' @examples
 #'   stw_to_table(diamonds_meta)
 #'
-#'
-stw_to_table <- function(meta){
+stw_to_table <- function(meta) {
 
   meta[["name"]] <- toupper(meta[["name"]])
+
   meta$dictionary %>%
     janitor::clean_names("upper_camel") %>%
     gt::gt() %>%
@@ -19,10 +19,9 @@ stw_to_table <- function(meta){
       title = meta[["name"]],
       subtitle = meta[["title"]]
     ) %>%
-    gt::tab_source_note(source_note=meta[["source"]]) %>%
+    gt::tab_source_note(source_note = meta[["source"]]) %>%
     gt::tab_style(
-      style = gt::cells_styles(
-      text_style = "italic"),
+      style = gt::cells_styles(text_style = "italic"),
       locations = gt::cells_data("Name")
     )
 }
