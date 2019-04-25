50dbb676c83266f206f1dc4e3e2a81016537f0dc
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-04-25 10:57:10 -0500

    Add testing, roxygen-string sanitization, tweak yaml

---
+++ tests/testthat/test-stw_to_roxygen.R
@@ -0,0 +1,29 @@
+
+
+test_that("roxygen substitution works", {
+
+  expect_roxygen <- function(x, y) {
+    testthat::expect_identical(
+      roxygen_substitute(x),
+      glue::as_glue(y)
+    )
+  }
+
+  # no-op
+  expect_roxygen("a", "a")
+
+  # single `@` replaced with `@@`
+  expect_roxygen("ian.lyttle@se.com", "ian.lyttle@@se.com")
+
+  # no-op for roxygen tags
+  expect_roxygen("#' @format", "#' @format")
+
+  # no-op if already doubled
+  expect_roxygen("@@", "@@")
+
+  # escape `%`
+  expect_roxygen("%", "\\%")
+
+  # no-op if already escaped
+  expect_roxygen("\\%", "\\%")
+})
