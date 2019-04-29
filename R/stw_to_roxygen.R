90b6b705b50bb7fb38442c6762f0905ab8f059d8
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-04-29 09:05:43 -0600

    Remake stw_to_roxygen() as S3 generic

--- R/stw_to_roxygen.R
+++ R/stw_to_roxygen.R
@@ -2,17 +2,17 @@
 #'
 #' Use these functions to convert your metadata to roxygen format. The function
 #' `stw_to_roxygen()` returns a roxygen string, `stw_use_roxygen()` copies a
-#' roxygen string to your clipboard, and `str_write_yaml()` writes a roxygen
+#' roxygen string to your clipboard, and `str_write_roxygen()` writes a roxygen
 #' string to a file.
 #'
-#' @param meta object with S3 class `stw_meta`
+#' @param x object with S3 class `stw_meta`
 #' @param file `character` path of file to write
 #'
 #' @return
 #' \describe{
 #'   \item{`stw_to_roxygen()`}{`character`, roxygen string}
-#'   \item{`stw_use_roxygen()`}{`invisible(meta)`, called for side-effects}
-#'   \item{`stw_write_roxygen()`}{`invisible(meta)`, called for side-effects}
+#'   \item{`stw_use_roxygen()`}{`invisible(x)`, called for side-effects}
+#'   \item{`stw_write_roxygen()`}{`invisible(x)`, called for side-effects}
 #' }
 #' @export
 #'
@@ -24,27 +24,46 @@
 #' stw_use_roxygen(diamonds_meta)
 #' }
 #'
-stw_to_roxygen <- function(meta) {
+stw_to_roxygen <- function(x, ...) {
+  UseMethod("stw_to_roxygen")
+}
+
+#' @rdname stw_to_roxygen
+#' @export
+#'
+stw_to_roxygen.default <- function(x, ...) {
+  stop(
+    glue::glue(
+      "{usethis::ui_code('stw_to_roxygen()')} does not have a method ",
+      "for objects of class {usethis::ui_code(class(x))}"
+    )
+  )
+}
+
+#' @rdname stw_to_roxygen
+#' @export
+#'
+stw_to_roxygen.stw_meta <- function(x, ...) {
 
   top_bread <-
     glue::glue(
-        "#' {meta$title}",
+        "#' {x$title}",
         "#' ",
-        "#' {meta$description}",
+        "#' {x$description}",
         "#' ",
-        "#' @format A data frame with {meta$n_row} rows and {meta$n_col} variables:",
+        "#' @format A data frame with {x$n_row} rows and {x$n_col} variables:",
         "#' ",
         "#' \\describe{{ ",
         .sep = "\n"
     )
 
-  fillings <- dict_to_roxygen(meta$dictionary)
+  fillings <- dict_to_roxygen(x$dictionary)
 
   bottom_bread <-
     glue::glue(
       "#' }}",
-      "#' @source {meta$source}",
-      "\"{meta$name}\"",
+      "#' @source {x$source}",
+      "\"{x$name}\"",
       .sep = "\n"
     )
 
@@ -58,6 +77,38 @@ stw_to_roxygen <- function(meta) {
   sandwich <- roxygen_substitute(sandwich)
 
   as.character(sandwich)
+}
+
+#' @rdname stw_to_roxygen
+#' @export
+#'
+stw_use_roxygen <- function(x) {
+
+  roxygen <- stw_to_roxygen(x)
+
+  # escape curly-brackets
+  roxygen <- stringr::str_replace_all(roxygen, "\\{", "{{")
+  roxygen <- stringr::str_replace_all(roxygen, "\\}", "}}")
+
+  usethis::ui_code_block(roxygen)
+  usethis::ui_todo("Paste this text into a file; be sure to end the file with a newline character.")
+
+  invisible(x)
+}
+
+#' @rdname stw_to_roxygen
+#' @export
+#'
+stw_write_roxygen <- function(x, file) {
+
+  roxygen <- stw_to_roxygen(x)
+
+  roxygen <- paste0(roxygen, "\n\n") # add newlines
+  readr::write_file(roxygen, file)
+
+  usethis::ui_done("Roxygen metadata written to {usethis::ui_value(file)}.")
+
+  invisible(x)
 }
 
 
@@ -89,38 +140,6 @@ roxygen_substitute <- function(x) {
   x <- stringr::str_replace_all(x, "(?<!\\\\)%", "\\\\%")
 
   x
-}
-
-#' @rdname stw_to_roxygen
-#' @export
-#'
-stw_use_roxygen <- function(meta) {
-
-  roxygen <- stw_to_roxygen(meta)
-
-  # escape curly-brackets
-  roxygen <- stringr::str_replace_all(roxygen, "\\{", "{{")
-  roxygen <- stringr::str_replace_all(roxygen, "\\}", "}}")
-
-  usethis::ui_code_block(roxygen)
-  usethis::ui_todo("Paste this text into a file; be sure to end the file with a newline character.")
-
-  invisible(meta)
-}
-
-#' @rdname stw_to_roxygen
-#' @export
-#'
-stw_write_roxygen <- function(meta, file) {
-
-  roxygen <- stw_to_roxygen(meta)
-
-  roxygen <- paste0(roxygen, "\n\n") # add newlines
-  readr::write_file(roxygen, file)
-
-  usethis::ui_done("Roxygen metadata written to {usethis::ui_value(file)}.")
-
-  invisible(meta)
 }
 
 
