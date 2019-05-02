f5ce73e7e438be9c67b14494a9675214ef5dfa52
Ian Lyttle <ijlyttle@users.noreply.github.com>
2019-05-02 10:16:41 -0600

    Validate (#62)
    * Start framework for validation
    
    * Begin framework
    
    * Make dictionary validation
    
    * Complete testing for stw_check.stw_dict
    
    * Get stw_check working for stw_meta
    
    * Complete stw_validate() functions, update documentation
    
    * Update pkgdown

---
+++ R/stw_validate.R
@@ -0,0 +1,274 @@
+#' Check steward object
+#'
+#' Use these function to determine the validity of a `stw_dict` or `stw_meta` object;
+#' `stw_check()` tells you about the object, `stw_validate()` additionally throws
+#' an error if warranted. To support piping, these functions return their first
+#' argument invisibly.
+#'
+#' You can specify the `verbosity`:
+#'
+#' \describe{
+#'   \item{`"error"`}{reports results of all checks that failed}
+#'   \item{`"info"`}{reports results of all checks that failed,
+#'     and that find missing optional information}
+#'   \item{`"all"`}{reports results of all checks}
+#'   \item{`"none"`}{reports no results}
+#' }
+#'
+#' The `"none"` option may not seem intuitive, it is for internal use.
+#' These functions set an internal attribute of the object to inicate its
+#' validity.
+#'
+#' For a `stw_dict` object, each `name` nust be unique and non-trivial,
+#' each `description` must be non-trivial, `type` is optional.
+#'
+#' For a `stw_meta` object, it must have a non-trivial `name`, and `dict`.
+#' It may have a `title`, `description`, `source`, `n_row`, and `n_col`.
+#'
+#' @param ... other arguments (not used)
+#' @param meta object with S3 class `stw_meta`,
+#'   contains dataset metadata; see [stw_meta()].
+#' @param dict object with S3 class `stw_dict`,
+#'   contains dataset data-dictionary; see [stw_dict()].
+#' @param verbosity `character`, determines the amount of feedback, see Details.
+#'
+#' @return modified copy of `dict` or `meta`
+#' @export
+#'
+#' @examples
+#' stw_check(diamonds_meta$dict)
+#'
+stw_check <- function(...) {
+  UseMethod("stw_check")
+}
+
+#' @rdname stw_check
+#' @export
+#'
+stw_check.default <- function(...) {
+  dots <- rlang::list2(...)
+  stop(error_message_method("stw_check()", class(dots[[1]])))
+}
+
+#' @rdname stw_check
+#' @export
+#'
+stw_check.stw_dict <- function(dict,
+                               verbosity = c("error", "info", "all", "none"),
+                               ...) {
+  verbosity <- match.arg(verbosity)
+  ui_fn <- get_ui_functions(verbosity)
+
+  valid <- TRUE
+
+  # names are unique
+  names_repeated <- dict[["name"]][duplicated(dict[["name"]])]
+  if (length(names_repeated) > 0) {
+    valid <- FALSE
+    ui_fn$ui_oops(
+      "Dictionary names are repeated: {usethis::ui_value(names_repeated)}."
+    )
+  } else {
+    ui_fn$ui_done("Dictionary names are unique.")
+  }
+
+  # names are non-trivial
+  names_trivial <- dict[["name"]][stringr::str_detect(dict[["name"]], "^\\s*$")]
+  if (length(names_trivial) > 0) {
+    valid <- FALSE
+    ui_fn$ui_oops(
+      "Dictionary names are missing."
+    )
+  } else {
+    ui_fn$ui_done("Dictionary names are all non-trivial.")
+  }
+
+  # descriptions are non-trivial
+  desc_trivial <-
+    dict[["name"]][stringr::str_detect(dict[["description"]], "^\\s*$")]
+  if (length(desc_trivial) > 0) {
+    valid <- FALSE
+    ui_fn$ui_oops(
+      "Dictionary descriprions are missing for names: {usethis::ui_value(desc_trivial)}."
+    )
+  } else {
+    ui_fn$ui_done("Dictionary descriptions are all non-trivial.")
+  }
+
+  # types are non-trivial
+  type_trivial <-
+    dict[["name"]][stringr::str_detect(dict[["type"]], "^\\s*$")]
+  if (length(type_trivial) > 0) {
+    ui_fn$ui_info(
+      "Dictionary types are trivial for names: {usethis::ui_value(type_trivial)}."
+    )
+  } else {
+    ui_fn$ui_done("Dictionary types are all non-trivial.")
+  }
+
+  # set the validity
+  dict <- set_valid(dict, valid)
+
+  invisible(dict)
+}
+
+
+#' @rdname stw_check
+#' @export
+#'
+stw_check.stw_meta <- function(meta,
+                               verbosity = c("error", "info", "all", "none"),
+                               ...) {
+
+  verbosity <- match.arg(verbosity)
+  ui_fn <- get_ui_functions(verbosity)
+
+  # check the dictionary
+  meta[["dict"]] <- stw_check(meta[["dict"]], verbosity = verbosity)
+  valid <- get_valid(meta[["dict"]])
+
+  # check the fields
+  fields <- names(meta)
+
+  # check the mandatory fields
+  mandatory <- c("name", "dict")
+  missing_mandatory <- mandatory[!check_elements(mandatory, meta)]
+
+  if (length(missing_mandatory) > 0) {
+    valid <- FALSE
+    ui_fn$ui_oops("Metadata missing required fields: {usethis::ui_value(missing_mandatory)}.")
+  } else {
+    ui_fn$ui_done("Metadata has all required fields.")
+  }
+
+  # check the optional fields
+  optional <- c("title", "description", "source", "n_row", "n_col")
+  missing_optional <- optional[!check_elements(optional, meta)]
+
+  if (length(missing_optional) > 0) {
+    ui_fn$ui_info("Metadata missing optional fields: {usethis::ui_value(missing_optional)}.")
+  } else {
+    ui_fn$ui_done("Metadata has all optional fields.")
+  }
+
+  # set the validity
+  meta <- set_valid(meta, valid)
+
+  invisible(meta)
+}
+
+#' @rdname stw_check
+#' @export
+#'
+stw_validate <- function(...) {
+  UseMethod("stw_validate")
+}
+
+#' @rdname stw_check
+#' @export
+#'
+stw_validate.default <- function(...) {
+  dots <- rlang::list2(...)
+  stop(error_message_method("stw_validate()", class(dots[[1]])))
+}
+
+#' @rdname stw_check
+#' @export
+#'
+stw_validate.stw_dict <- function(dict,
+                                  verbosity = c("error", "info", "all"),
+                                  ...) {
+
+  verbosity <- match.arg(verbosity)
+
+  dict <- stw_check(dict, verbosity = verbosity)
+
+  if (!get_valid(dict)) {
+    stop("Dictionary not valid.")
+  }
+
+  invisible(dict)
+}
+
+#' @rdname stw_check
+#' @export
+#'
+stw_validate.stw_meta <- function(meta,
+                                  verbosity = c("error", "info", "all"),
+                                  ...) {
+
+  verbosity <- match.arg(verbosity)
+
+  meta <- stw_check(meta, verbosity = verbosity)
+
+  if (!get_valid(meta)) {
+    stop("Metadata not valid.")
+  }
+
+  invisible(meta)
+}
+
+get_valid <- function(x) {
+  identical(attr(x, "is_valid"), TRUE)
+}
+
+set_valid <- function(x, valid) {
+  attr(x, "is_valid") <- identical(valid, TRUE)
+  x
+}
+
+get_ui_functions <- function(verbosity = c("none", "error", "info", "all")) {
+
+  verbosity <- match.arg(verbosity)
+
+  write_null <- function(x, ...) {
+    NULL
+  }
+
+  set <- list(
+    ui_oops = usethis::ui_oops,
+    ui_info = usethis::ui_info,
+    ui_done = usethis::ui_done
+  )
+
+  if (identical(verbosity, "info")) {
+    set[["ui_done"]] <- write_null
+  }
+
+  if (identical(verbosity, "error")) {
+    set[["ui_done"]] <- write_null
+    set[["ui_info"]] <- write_null
+  }
+
+  if (identical(verbosity, "none")) {
+    set[["ui_done"]] <- write_null
+    set[["ui_info"]] <- write_null
+    set[["ui_oops"]] <- write_null
+  }
+
+  set
+}
+
+# return a named vector indicating validity
+check_elements <- function(names, item) {
+
+  is_valid <- function(name, item) {
+
+    elem <- item[[name]]
+
+    if (is.data.frame(elem)) {
+      return(TRUE)
+    }
+
+    valid <-
+      !is.null(elem) &&
+      !is.na(elem) &&
+      length(elem) > 0 &&
+      !stringr::str_detect(elem, "^\\s*$") &&
+      TRUE
+
+    valid
+  }
+
+  vapply(names, is_valid, FUN.VALUE = logical(1), item)
+}
---
+++ tests/testthat/test-stw_validate.R
@@ -0,0 +1,116 @@
+library("tibble")
+
+dict_good <- tribble(
+  ~name, ~type,     ~description,
+  "x",   "integer", "count",
+  "y",   "integer", "another count"
+)
+
+dict_good <- stw_dict(dict_good)
+
+dict_names_repeated <-
+  add_row(dict_good, name = "x", type = "integer", description = "yolo")
+
+dict_names_trivial <-
+  add_row(dict_good, name = " ", type = "integer", description = "yolo")
+
+dict_type_trivial <-
+  add_row(dict_good, name = "z", type = "", description = "yolo")
+
+dict_desc_trivial <-
+  add_row(dict_good, name = "z", type = "integer", description = "")
+
+meta_good <- diamonds_meta
+
+meta_missing_name <- meta_good
+meta_missing_name$name <- NULL
+
+meta_missing_source <- meta_good
+meta_missing_source$source <- " "
+
+test_that("check works for dictionary", {
+
+  get_valid <- function(x) {
+    identical(attr(x, "is_valid"), TRUE)
+  }
+
+  # check we get the right result
+  expect_true(get_valid(stw_check(dict_good)))
+  expect_true(get_valid(stw_check(dict_type_trivial)))
+  expect_false(get_valid(stw_check(dict_names_repeated)))
+  expect_false(get_valid(stw_check(dict_names_trivial)))
+  expect_false(get_valid(stw_check(dict_desc_trivial)))
+
+  # check we reutrn the item
+  expect_equivalent(stw_check(dict_good), dict_good)
+})
+
+test_that("check works for dictionary", {
+
+  get_valid <- function(x) {
+    identical(attr(x, "is_valid"), TRUE)
+  }
+
+  # check we get the right result
+  expect_true(get_valid(stw_check(meta_good)))
+  expect_true(get_valid(stw_check(meta_missing_source)))
+  expect_false(get_valid(stw_check(meta_missing_name)))
+
+  # check we reutrn the item
+  expect_equivalent(stw_check(meta_good), meta_good)
+})
+
+test_that("check side-effects are correct for dictionary", {
+  # everything OK
+  expect_output(stw_check(meta_good, verbosity = "all"))
+  expect_silent(stw_check(meta_good, verbosity = "info"))
+  expect_silent(stw_check(meta_good, verbosity = "error"))
+  expect_silent(stw_check(meta_good, verbosity = "none"))
+
+  # missing type
+  expect_output(stw_check(meta_missing_source, verbosity = "all"))
+  expect_output(stw_check(meta_missing_source, verbosity = "info"))
+  expect_silent(stw_check(meta_missing_source, verbosity = "error"))
+  expect_silent(stw_check(meta_missing_source, verbosity = "none"))
+
+  # repeated name
+  expect_output(stw_check(meta_missing_name, verbosity = "all"))
+  expect_output(stw_check(meta_missing_name, verbosity = "info"))
+  expect_output(stw_check(meta_missing_name, verbosity = "error"))
+  expect_silent(stw_check(meta_missing_name, verbosity = "none"))
+})
+
+test_that("check side-effects are correct for meta", {
+  # everything OK
+  expect_output(stw_check(dict_good, verbosity = "all"))
+  expect_silent(stw_check(dict_good, verbosity = "info"))
+  expect_silent(stw_check(dict_good, verbosity = "error"))
+  expect_silent(stw_check(dict_good, verbosity = "none"))
+
+  # missing type
+  expect_output(stw_check(dict_type_trivial, verbosity = "all"))
+  expect_output(stw_check(dict_type_trivial, verbosity = "info"))
+  expect_silent(stw_check(dict_type_trivial, verbosity = "error"))
+  expect_silent(stw_check(dict_type_trivial, verbosity = "none"))
+
+  # repeated name
+  expect_output(stw_check(dict_names_repeated, verbosity = "all"))
+  expect_output(stw_check(dict_names_repeated, verbosity = "info"))
+  expect_output(stw_check(dict_names_repeated, verbosity = "error"))
+  expect_silent(stw_check(dict_names_repeated, verbosity = "none"))
+})
+
+test_that("validate works", {
+
+  # silent
+  expect_silent(stw_validate(dict_good))
+  expect_silent(stw_validate(meta_good))
+
+  # returns argument
+  expect_equivalent(stw_validate(dict_good), dict_good)
+  expect_equivalent(stw_validate(meta_good), meta_good)
+
+  # throws error
+  expect_error(stw_validate(dict_names_repeated))
+  expect_error(stw_validate(meta_missing_name))
+})
