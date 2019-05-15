532cba4a95aa536991a47e297be3c643be3f2f87
Ian Lyttle <ian.lyttle@schneider-electric.com>
2019-05-15 15:30:29 -0500

    Gets table-schema working for read_yaml, #68

--- R/stw_read_meta_yaml.R
+++ R/stw_read_meta_yaml.R
@@ -14,7 +14,23 @@ stw_read_meta_yaml <- function(file) {
   # read in the yaml file
   infile <- yaml::read_yaml(file)
 
-  dict <- lapply(infile$dict, function(x) {do.call(stw_dict, x)})
+  # move schema$fields to dict
+  dict <- infile[["schema"]][["fields"]]
+
+  infile$schema <- NULL
+
+  # move constraints$enum to levels
+  to_level <- function(x) {
+
+    levels <- x[["constraints"]][["enum"]]
+    x[["constraints"]] <- NULL
+    x[["levels"]] <- levels
+
+    x
+  }
+
+  dict <- lapply(dict, to_level)
+  dict <- lapply(dict, function(x) {do.call(stw_dict, x)})
 
   dict <- do.call(rbind, dict) # this combines all the different values into 1 tibble
 
