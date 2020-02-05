560a3a635859ee249b2a0ff10dcec557deb60143
Ian Lyttle <ian.lyttle@schneider-electric.com>
2020-02-04 21:46:56 -0600

    remove failing test

--- tests/testthat/test-stw_read_write_dataset.R
+++ tests/testthat/test-stw_read_write_dataset.R
@@ -13,7 +13,6 @@ diamonds$clarity <- factor(diamonds$clarity, ordered = FALSE)
 diamonds <- stw_dataset(diamonds, diamonds_meta)
 
 test_that("we can write a dataset", {
-  expect_true(FALSE)
   expect_output(
     expect_identical(stw_write_dataset(diamonds, file_data), diamonds)
   )
