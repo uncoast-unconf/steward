1116f7e64907cf0ce566480c062aef9bb132fd74
Ian Lyttle <ian.lyttle@schneider-electric.com>
2020-07-15 20:33:22 -0500

    update tests

--- tests/testthat/test-col_spec.R
+++ tests/testthat/test-col_spec.R
@@ -3,8 +3,8 @@
 library("dplyr")
 library("readr")
 
-col_spec_iris <- as.col_spec(iris)
-col_spec_iris_null <- col_spec_select(col_spec_iris)
+col_spec_mtcars <- as.col_spec(mtcars)
+col_spec_mtcars_null <- col_spec_select(col_spec_mtcars)
 
 result_identical <-
   col_spec_diff(
@@ -12,19 +12,19 @@ result_identical <-
     names = list(
       identical = TRUE,
       equivalent = TRUE,
-      x_not_y = col_spec_iris_null,
-      y_not_x = col_spec_iris_null
+      x_not_y = col_spec_mtcars_null,
+      y_not_x = col_spec_mtcars_null
     ),
     specs_common = list(
       identical = TRUE,
-      x_diff_y = col_spec_iris_null,
-      y_diff_x = col_spec_iris_null
+      x_diff_y = col_spec_mtcars_null,
+      y_diff_x = col_spec_mtcars_null
     )
   )
 
-iris_missing_cols <-
-  iris %>%
-  select(-Sepal.Length)
+mtcars_missing_cols <-
+  mtcars %>%
+  select(-mpg)
 
 result_missing_cols <-
   col_spec_diff(
@@ -32,19 +32,19 @@ result_missing_cols <-
     names = list(
       identical = FALSE,
       equivalent = FALSE,
-      x_not_y = col_spec_iris_null,
-      y_not_x = col_spec_select(col_spec_iris, "Sepal.Length")
+      x_not_y = col_spec_mtcars_null,
+      y_not_x = col_spec_select(col_spec_mtcars, "mpg")
     ),
     specs_common = list(
       identical = TRUE,
-      x_diff_y = col_spec_iris_null,
-      y_diff_x = col_spec_iris_null
+      x_diff_y = col_spec_mtcars_null,
+      y_diff_x = col_spec_mtcars_null
     )
   )
 
-iris_extra_cols <-
-  iris %>%
-  mutate(Specieso = Species)
+mtcars_extra_cols <-
+  mtcars %>%
+  mutate(cylo = cyl)
 
 result_extra_cols <-
   col_spec_diff(
@@ -52,20 +52,20 @@ result_extra_cols <-
     names = list(
       identical = FALSE,
       equivalent = FALSE,
-      x_not_y = as.col_spec(iris_extra_cols) %>% col_spec_select("Specieso"),
-      y_not_x = col_spec_iris_null
+      x_not_y = as.col_spec(mtcars_extra_cols) %>% col_spec_select("cylo"),
+      y_not_x = col_spec_mtcars_null
     ),
     specs_common = list(
       identical = TRUE,
-      x_diff_y = col_spec_iris_null,
-      y_diff_x = col_spec_iris_null
+      x_diff_y = col_spec_mtcars_null,
+      y_diff_x = col_spec_mtcars_null
     )
   )
 
-iris_missing_extra_cols <-
-  iris %>%
-  select(-Sepal.Length) %>%
-  mutate(Specieso = Species)
+mtcars_missing_extra_cols <-
+  mtcars %>%
+  select(-mpg) %>%
+  mutate(cylo = cyl)
 
 result_missing_extra_cols <-
   col_spec_diff(
@@ -73,19 +73,19 @@ result_missing_extra_cols <-
     names = list(
       identical = FALSE,
       equivalent = FALSE,
-      x_not_y = as.col_spec(iris_extra_cols) %>% col_spec_select("Specieso"),
-      y_not_x = col_spec_select(col_spec_iris, "Sepal.Length")
+      x_not_y = as.col_spec(mtcars_extra_cols) %>% col_spec_select("cylo"),
+      y_not_x = col_spec_select(col_spec_mtcars, "mpg")
     ),
     specs_common = list(
       identical = TRUE,
-      x_diff_y = col_spec_iris_null,
-      y_diff_x = col_spec_iris_null
+      x_diff_y = col_spec_mtcars_null,
+      y_diff_x = col_spec_mtcars_null
     )
   )
 
-iris_wrong_class <-
-  iris %>%
-  mutate(Species = as.character(Species))
+mtcars_wrong_class <-
+  mtcars %>%
+  mutate(disp = as.character(disp))
 
 result_wrong_class <-
   col_spec_diff(
@@ -93,19 +93,19 @@ result_wrong_class <-
     names = list(
       identical = TRUE,
       equivalent = TRUE,
-      x_not_y = col_spec_iris_null,
-      y_not_x = col_spec_iris_null
+      x_not_y = col_spec_mtcars_null,
+      y_not_x = col_spec_mtcars_null
     ),
     specs_common = list(
       identical = FALSE,
-      x_diff_y = as.col_spec(iris_wrong_class) %>% col_spec_select("Species"),
-      y_diff_x = col_spec_select(col_spec_iris, "Species")
+      x_diff_y = as.col_spec(mtcars_wrong_class) %>% col_spec_select("disp"),
+      y_diff_x = col_spec_select(col_spec_mtcars, "disp")
     )
   )
 
-iris_diff_order <-
-  iris %>%
-  select(Species, everything())
+mtcars_diff_order <-
+  mtcars %>%
+  select(cyl, everything())
 
 result_diff_order <-
   col_spec_diff(
@@ -113,19 +113,19 @@ result_diff_order <-
     names = list(
       identical = FALSE,
       equivalent = TRUE,
-      x_not_y = col_spec_iris_null,
-      y_not_x = col_spec_iris_null
+      x_not_y = col_spec_mtcars_null,
+      y_not_x = col_spec_mtcars_null
     ),
     specs_common = list(
       identical = TRUE,
-      x_diff_y = col_spec_iris_null,
-      y_diff_x = col_spec_iris_null
+      x_diff_y = col_spec_mtcars_null,
+      y_diff_x = col_spec_mtcars_null
     )
   )
 
-iris_missing_extra_cols_wrong_class <-
-  iris_missing_extra_cols %>%
-  mutate(Species = as.character(Species))
+mtcars_missing_extra_cols_wrong_class <-
+  mtcars_missing_extra_cols %>%
+  mutate(disp = as.character(disp))
 
 result_missing_extra_cols_wrong_class <-
   col_spec_diff(
@@ -133,100 +133,100 @@ result_missing_extra_cols_wrong_class <-
     names = list(
       identical = FALSE,
       equivalent = FALSE,
-      x_not_y = as.col_spec(iris_extra_cols) %>% col_spec_select("Specieso"),
-      y_not_x = col_spec_select(col_spec_iris, "Sepal.Length")
+      x_not_y = as.col_spec(mtcars_extra_cols) %>% col_spec_select("cylo"),
+      y_not_x = col_spec_select(col_spec_mtcars, "mpg")
     ),
     specs_common = list(
       identical = FALSE,
-      x_diff_y = as.col_spec(iris_wrong_class) %>% col_spec_select("Species"),
-      y_diff_x = col_spec_select(col_spec_iris, "Species")
+      x_diff_y = as.col_spec(mtcars_wrong_class) %>% col_spec_select("disp"),
+      y_diff_x = col_spec_select(col_spec_mtcars, "disp")
     )
   )
 
-result_mtcars <-
+result_cars <-
   col_spec_diff(
     identical = FALSE,
     names = list(
       identical = FALSE,
       equivalent = FALSE,
-      x_not_y = col_spec_iris,
-      y_not_x = as.col_spec(mtcars)
+      x_not_y = col_spec_mtcars,
+      y_not_x = as.col_spec(cars)
     ),
     specs_common = list(
       identical = as.logical(NA),
-      x_diff_y = col_spec_iris_null,
-      y_diff_x = col_spec_select(as.col_spec(mtcars))
+      x_diff_y = col_spec_mtcars_null,
+      y_diff_x = col_spec_select(as.col_spec(cars))
     )
   )
 
-test_that("iris checks out with itself", {
+test_that("mtcars checks out with itself", {
 
   expect_identical(
-    col_spec_compare(iris, iris),
+    col_spec_compare(mtcars, mtcars),
     result_identical
   )
 
 })
 
-test_that("iris checks out with missing column", {
+test_that("mtcars checks out with missing column", {
 
   expect_identical(
-    col_spec_compare(iris_missing_cols, iris),
+    col_spec_compare(mtcars_missing_cols, mtcars),
     result_missing_cols
   )
 
 })
 
-test_that("iris checks out with extra column", {
+test_that("mtcars checks out with extra column", {
 
   expect_identical(
-    col_spec_compare(iris_extra_cols, iris),
+    col_spec_compare(mtcars_extra_cols, mtcars),
     result_extra_cols
   )
 
 })
 
-test_that("iris checks out with missing and extra column", {
+test_that("mtcars checks out with missing and extra column", {
 
   expect_identical(
-    col_spec_compare(iris_missing_extra_cols, iris),
+    col_spec_compare(mtcars_missing_extra_cols, mtcars),
     result_missing_extra_cols
   )
 
 })
 
-test_that("iris checks out with wrong class", {
+test_that("mtcars checks out with wrong class", {
 
   expect_identical(
-    col_spec_compare(iris_wrong_class, iris),
+    col_spec_compare(mtcars_wrong_class, mtcars),
     result_wrong_class
   )
 
 })
 
-test_that("iris checks out with different order", {
+test_that("mtcars checks out with different order", {
 
   expect_identical(
-    col_spec_compare(iris_diff_order, iris),
+    col_spec_compare(mtcars_diff_order, mtcars),
     result_diff_order
   )
 
 })
 
-test_that("iris checks out with all problems", {
+test_that("mtcars checks out with all problems", {
 
   expect_identical(
-    col_spec_compare(iris_missing_extra_cols_wrong_class, iris),
+    col_spec_compare(mtcars_missing_extra_cols_wrong_class, mtcars),
     result_missing_extra_cols_wrong_class
   )
 
 })
 
-test_that("iris checks out with mtcars", {
+test_that("mtcars checks out with mtcars", {
 
   expect_identical(
-    col_spec_compare(iris, mtcars),
-    result_mtcars
+    col_spec_compare(mtcars, cars),
+    result_cars
   )
 
 })
@@ -241,7 +241,7 @@ test_that("col_spec_diff printing works", {
     result_diff_order
     result_wrong_class
     result_missing_extra_cols_wrong_class
-    result_mtcars
+    result_cars
   })
 
 })
@@ -249,21 +249,21 @@ test_that("col_spec_diff printing works", {
 test_that("col_spec_select works", {
 
   expect_identical(
-    as.col_spec(iris) %>% col_spec_select(-Sepal.Length),
-    as.col_spec(iris_missing_cols)
+    as.col_spec(mtcars) %>% col_spec_select(-mpg),
+    as.col_spec(mtcars_missing_cols)
   )
 
   expect_identical(
-    as.col_spec(iris) %>%
+    as.col_spec(mtcars) %>%
       col_spec_select(
-        c("Sepal.Width", "Petal.Length", "Petal.Width", "Species")
+        c("cyl", "disp", "hp", "drat", "wt", "qsec", "vs", "am", "gear", "carb")
       ),
-    as.col_spec(iris_missing_cols)
+    as.col_spec(mtcars_missing_cols)
   )
 
   expect_identical(
-    as.col_spec(iris) %>% col_spec_select(seq(2, 5)),
-    as.col_spec(iris_missing_cols)
+    as.col_spec(mtcars) %>% col_spec_select(seq(2, 11)),
+    as.col_spec(mtcars_missing_cols)
   )
 
 })
